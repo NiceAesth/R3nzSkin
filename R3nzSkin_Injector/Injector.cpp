52903125e32a9ba01a634323577083a85333410a
NiceAesth <nice@aesth.dev>
2023-06-28 17:28:42 +0300

    style: improve code style of auto-updater

--- R3nzSkin_Injector/Injector.cpp
+++ R3nzSkin_Injector/Injector.cpp
@@ -159,61 +159,53 @@ void Injector::autoUpdate()
 	WebClient^ client = gcnew WebClient();
 	client->Headers->Add(L"User-Agent", L"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0");
 
-	try
-	{
-		std::string json = msclr::interop::marshal_as<std::string>(client->DownloadString(L"https://api.github.com/repos/R3nzTheCodeGOD/R3nzSkin/releases/latest"));
+	try {
+		std::string json = msclr::interop::marshal_as<std::string>(client->DownloadString(L"https://api.github.com/repos/NiceAesth/R3nzSkin/releases/latest"));
 		std::regex tagnameRegex("\"tag_name\"\\s*:\\s*\"([^\"]+)");
 		std::regex urlRegex("\"browser_download_url\"\\s*:\\s*\"([^\"]+)");
 		std::regex dateRegex("\"created_at\"\\s*:\\s*\"([^\"]+)");
 
 		std::smatch tagnameMatch, urlMatch, dateMatch;
-		if (std::regex_search(json, tagnameMatch, tagnameRegex))
-		{
+		if (std::regex_search(json, tagnameMatch, tagnameRegex)) {
 			auto version = gcnew String(tagnameMatch[1].str().c_str());
-			if (std::regex_search(json, dateMatch, dateRegex))
-			{
+			if (std::regex_search(json, dateMatch, dateRegex)) {
 				DateTime date_of_github_release = DateTime::ParseExact(gcnew String(dateMatch[1].str().c_str()), L"yyyy-MM-ddTHH:mm:ss'Z'", CultureInfo::InvariantCulture);
 				DateTime date_of_current_release = System::IO::File::GetLastWriteTime(L"R3nzSkin.dll");
-				if (date_of_current_release != date_of_github_release)
-				{
-					if (date_of_current_release > date_of_github_release)
-					{
-						return;
-					}
+				if (date_of_current_release == date_of_github_release)
+					return;
 
-					auto result = MessageBox::Show(L"New version is available on GitHub\nWould you like to download it now?", L"R3nzSkin", MessageBoxButtons::YesNo, MessageBoxIcon::Information);
-					if (result == DialogResult::Yes)
-					{
-						if (std::regex_search(json, urlMatch, urlRegex))
-						{
-							auto url = gcnew String(urlMatch[1].str().c_str());
-							auto file = String::Format(L"R3nzSkin_{0}.zip", version);
-							client->DownloadFile(url, file);
+				if (date_of_current_release > date_of_github_release)
+					return;
 
-							System::IO::Compression::ZipFile::ExtractToDirectory(file, L"R3nzSkin");
-							System::IO::File::Delete(file);
-							System::IO::File::Delete(L"R3nzSkin.dll");
-							System::IO::File::Move(L"R3nzSkin\\R3nzSkin_Injector.exe", String::Format(L"R3nzSkin_Injector_{0}.exe", version));
-							System::IO::File::Move(L"R3nzSkin\\R3nzSkin.dll", L"R3nzSkin.dll");
-							System::IO::Directory::Delete(L"R3nzSkin");
+				auto result = MessageBox::Show(L"New version is available on GitHub\nWould you like to download it now?", L"R3nzSkin", MessageBoxButtons::YesNo, MessageBoxIcon::Information);
+				if (result == DialogResult::No)
+					return;
+
+				if (std::regex_search(json, urlMatch, urlRegex)) {
+					auto url = gcnew String(urlMatch[1].str().c_str());
+					auto file = String::Format(L"R3nzSkin_{0}.zip", version);
+					client->DownloadFile(url, file);
+
+					System::IO::Compression::ZipFile::ExtractToDirectory(file, L"R3nzSkin");
+					System::IO::File::Delete(file);
+					System::IO::File::Delete(L"R3nzSkin.dll");
+					System::IO::File::Move(L"R3nzSkin\\R3nzSkin_Injector.exe", String::Format(L"R3nzSkin_Injector_{0}.exe", version));
+					System::IO::File::Move(L"R3nzSkin\\R3nzSkin.dll", L"R3nzSkin.dll");
+					System::IO::Directory::Delete(L"R3nzSkin");
 							
-							auto process_info = gcnew System::Diagnostics::ProcessStartInfo();
-							process_info->Arguments = L"/C choice /C Y /N /D Y /T 1 & del \"" + System::Diagnostics::Process::GetCurrentProcess()->MainModule->FileName + L"\"";
-							process_info->CreateNoWindow = true;
-							process_info->FileName = L"cmd.exe";
-							process_info->WindowStyle = System::Diagnostics::ProcessWindowStyle::Hidden;
-							System::Diagnostics::Process::Start(process_info);
-							System::Diagnostics::Process::Start(String::Format(L"R3nzSkin_Injector_{0}.exe", version));
+					auto process_info = gcnew System::Diagnostics::ProcessStartInfo();
+					process_info->Arguments = L"/C choice /C Y /N /D Y /T 1 & del \"" + System::Diagnostics::Process::GetCurrentProcess()->MainModule->FileName + L"\"";
+					process_info->CreateNoWindow = true;
+					process_info->FileName = L"cmd.exe";
+					process_info->WindowStyle = System::Diagnostics::ProcessWindowStyle::Hidden;
+					System::Diagnostics::Process::Start(process_info);
+					System::Diagnostics::Process::Start(String::Format(L"R3nzSkin_Injector_{0}.exe", version));
 			
-							Environment::Exit(0);
-						}
-					}
+					Environment::Exit(0);
 				}
 			}
 		}
-	}
-	catch (Exception^ e)
-	{
+	} catch (Exception^ e) {
 		MessageBox::Show(e->Message, L"R3nzSkin", MessageBoxButtons::OK, MessageBoxIcon::Error);
 	}
 }
