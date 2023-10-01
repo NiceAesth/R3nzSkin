c24521b1f1a4d0d457e16e9cd02b1b3864691c87
NiceAesth <nice@aesth.dev>
2023-10-01 15:27:57 +0300

    chore: update sigs

--- R3nzSkin/memory.hpp
+++ R3nzSkin/memory.hpp
@@ -69,12 +69,7 @@ class Memory {
      0,
      &offsets::global::ManagerTemplate_AIMinionClient_},
     {{"48 8B 35 ? ? ? ? 4C 8D 44 24 ? 48 8B CE"}, true, false, true, 0, &offsets::global::ManagerTemplate_AITurret_},
-    {{"48 8B 0D ? ? ? ? FF 15 ? ? ? ? 48 8B 05 ? ? ? ? 39 98 ? ? ? ?"},
-     true,
-     false,
-     true,
-     0,
-     &offsets::global::Riot__g_window},
+    {{"48 8B 0D ? ? ? ? FF 15 ? ? ? ? 48 8B 05 ? ? ? ?"}, true, false, true, 0, &offsets::global::Riot__g_window},
     {{"48 8D 8B ? ? ? ? 48 89 44 24 ? C7 44 24"}, false, true, false, 0, &offsets::AIBaseCommon::CharacterDataStack},
     {{"40 38 BB ? ? ? ? 0F 85 ? ? ? ? 66 C7 83 ? ? ? ? ? ? 0F 31 48 C1 E2 20 4C 8D 83 ? ? ? ? 48 0B C2 44 8B CF 48 89 "
       "44 24 ? 8B D7"},
